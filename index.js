73aaa51104abd23405933cce2022664eed15bccb
BFerratto <bruferratto@gmail.com>
2017-03-18 17:27:39 -0300

    layout started

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var login = require ("./routes/login")
-var db = require("./config")
 var app = express()
-var port = db.port
+var port = 8085
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('src'));
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-app.post("/login",login)
 app.listen(port, function() {
-    console.log('App is listening on '+ port)
+    console.log('App is listening on http://localhost:'+ port)
 })
